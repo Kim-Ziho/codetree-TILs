6d15f3b481b007357a6f1e1c04778e50f350e3c0
Kim Jiho <82642592+Kim-Ziho@users.noreply.github.com>
2024-03-01 13:30:39 +0900

    Title: 바구니 안의 사탕, Time: 83ms, Memory: 4MB, Status: Runtime Error - CodeTree

---
+++ 240301/바구니 안의 사탕/candy-in-the-basket.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n, k;
+int arr[1000001];
+
+int main() {
+
+    cin >> n >> k;
+
+    for (int i = 0; i < n; i++) {
+        int num, pos;
+        cin >> num >> pos;
+        arr[pos] += num;
+    }
+
+    int c = k;
+    int sum = 0;
+    int i = 0;
+    int maxi = 0;
+
+    for (int i = 0; i <= c + k; i++) {
+        sum += arr[i];
+    }
+    maxi = sum;
+
+    for (int j = c + k + 1; j <= 1000000; j++) {
+        sum += arr[j] - arr[i];
+        maxi = max(maxi, sum);
+        i++;
+    }
+
+    cout << maxi;
+
+    return 0;
+}
