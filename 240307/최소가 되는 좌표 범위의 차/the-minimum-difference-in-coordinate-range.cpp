187bf6feb99bfa700e26dcd809728f604ea97873
Kim Jiho <82642592+Kim-Ziho@users.noreply.github.com>
2024-03-07 08:49:34 +0900

    Title: 최소가 되는 좌표 범위의 차, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240307/최소가 되는 좌표 범위의 차/the-minimum-difference-in-coordinate-range.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <algorithm>
+#include <climits>
+#include <set>
+
+#define MAX_N 100000
+
+using namespace std;
+
+int N, D;
+
+struct Coord {
+    int x;
+    int y;
+
+    bool operator<(const Coord& comp) const {
+        return y < comp.y;
+    }
+} arr[MAX_N];
+
+struct Compare_x {
+    bool operator()(const Coord& t, const Coord& v) const {
+        return t.x < v.x;
+    }
+};
+
+set<Coord> range;
+
+int get_min() {
+    if (range.empty()) return 0;
+    return range.begin()->y;
+}
+
+int get_max() {
+    if (range.empty()) return 0;
+    return range.rbegin()->y;
+}
+
+int main() {
+    // freopen("input.txt", "r", stdin);
+
+    cin >> N >> D;
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i].x >> arr[i].y;
+    }
+    sort (arr, arr + N, Compare_x());
+
+    int j = 0;
+    int ans = INT_MAX;
+
+    for (int i = 0; i < N - 1; i++) {
+        while (j < N && abs(get_min() - get_max()) < D) {
+            range.insert(arr[j]);
+            j++;
+        }
+        if (get_min() - get_max()) break;
+        ans = min(ans, arr[j].x - arr[i].x);
+        range.erase(range.find(arr[i]));
+    }
+
+    if (ans == INT_MAX) cout << -1 << endl;
+    else cout << ans << endl;
+
+    return 0;
+}
+
+/*
+2 4  6 12
+4 10 3 15
+
+10 7 3 5 13 17 3 6 9
+
+*/
