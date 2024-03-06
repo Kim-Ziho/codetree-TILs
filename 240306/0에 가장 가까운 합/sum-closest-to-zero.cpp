59185e7f9bfb419a07fd50d6ad929456e57e3599
Kim Jiho <82642592+Kim-Ziho@users.noreply.github.com>
2024-03-06 09:29:30 +0900

    Title: 0에 가장 가까운 합, Time: 52ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240306/0에 가장 가까운 합/sum-closest-to-zero.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+#include <climits>
+#include <cstdlib>
+
+#define MAX_N 100000
+
+using namespace std;
+
+int n;
+int arr[MAX_N + 1];
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    sort(arr, arr + n);
+
+    int j = n - 1;
+    int ans = INT_MAX;
+
+    for (int i = 0; i < n; i++) {
+        if (i == j) break;
+        while (j > 0 && abs(arr[i] + arr[j]) < ans) {
+            ans = abs(arr[i] + arr[j]);
+            j--;
+        }
+        j++;
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
+
+// -6 -5 -1 0 5 8 10
+// -123 1 1 2 2 124
