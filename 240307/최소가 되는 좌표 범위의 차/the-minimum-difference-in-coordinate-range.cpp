8eff8ace98f73d8aca31b7d0f15a1798944e2282
Kim Jiho <82642592+Kim-Ziho@users.noreply.github.com>
2024-03-07 11:02:43 +0900

    Title: 최소가 되는 좌표 범위의 차, Time: 6ms, Memory: 0MB, Status: Runtime Error - CodeTree

--- 240307/최소가 되는 좌표 범위의 차/the-minimum-difference-in-coordinate-range.cpp
+++ 240307/최소가 되는 좌표 범위의 차/the-minimum-difference-in-coordinate-range.cpp
@@ -1,68 +1,73 @@
 #include <iostream>
 #include <algorithm>
-#include <set>
 #include <climits>
+#include <set>
 
 #define MAX_N 100000
 
 using namespace std;
 
-// 변수 선언
-int n, d;
-pair<int, int> point[MAX_N + 1];
-set<pair<int, int> > point_count;
+int N, D;
+
+struct Coord {
+    int x;
+    int y;
+
+    bool operator<(const Coord& comp) const {
+        return y < comp.y;
+    }
+} arr[MAX_N];
+
+struct Compare_x {
+    bool operator()(const Coord& t, const Coord& v) const {
+        return t.x < v.x;
+    }
+};
+
+set<Coord> range;
 
 int get_min() {
-    if(point_count.empty()) return 0;
-    return point_count.begin()->first; 
+    if (range.empty()) return 0;
+    return range.begin()->y;
 }
 
 int get_max() {
-    if(point_count.empty()) return 0;
-    return point_count.rbegin()->first;
+    if (range.empty()) return 0;
+    return range.rbegin()->y;
 }
 
 int main() {
-    // 입력:
-    cin >> n >> d;
-    for(int i = 1; i <= n; i++) {
-        cin >> point[i].first >> point[i].second;
+    // freopen("input.txt", "r", stdin);
+
+    cin >> N >> D;
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i].x >> arr[i].y;
     }
+    sort (arr, arr + N, Compare_x());
 
-    sort(point + 1, point + n + 1);
-
-    // 가능한 구간 중 최소 크기를 구합니다.
+    int j = -1;
     int ans = INT_MAX;
 
-    // 구간을 잡아봅니다.
-    int j = 0;
-    for(int i = 1; i <= n; i++) {
-        // y좌표 차가 d가 되기 전까지 계속 진행합니다.
-        while(j + 1 <= n && get_max() - get_min() < d) {
-            point_count.insert({point[j + 1].second, point[j + 1].first});
+    for (int i = 0; i < N - 1; i++) {
+        while (j < N - 1 && get_max() - get_min() < D) {
             j++;
+            range.insert(arr[j]);
         }
-
-        // 만약 최대한 이동했는데도
-        // y좌표 차가 d가 되지 못했다면
-        // 탐색을 종료합니다.
-        if(get_max() - get_min() < d)
-            break;
-
-        // 현재 구간 [i, j]는 
-        // point[i]를 시작점으로 하는
-        // 가장 짧은 구간이므로
-        // 구간 크기 중 최솟값을 갱신합니다.
-        ans = min(ans, point[j].first-point[i].first);
-        
-        // 다음 구간으로 넘어가기 전에
-        // point[i]에 해당하는 값은 point_count에서 지워줍니다.
-        point_count.erase(point_count.find({point[i].second, point[i].first}));
+        if (get_max() - get_min() < D) break;
+        ans = min(ans, arr[j].x - arr[i].x);
+        range.erase(range.find(arr[i]));
     }
 
-    // 만약 불가능하다면
-    // -1을 답으로 합니다.
-    if(ans == INT_MAX) cout << -1;
-    else cout << ans;
+    if (ans == INT_MAX) cout << -1 << endl;
+    else cout << ans << endl;
+
     return 0;
 }
+
+/*
+2 4  6 12
+4 10 3 15
+
+10 7 3 5 13 17 3 6 9
+
+*/
