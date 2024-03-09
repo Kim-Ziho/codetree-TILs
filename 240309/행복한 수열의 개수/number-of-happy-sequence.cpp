f935de9b7277072e5efaa8ea0c91e4d385ee20a9
Kim Jiho <82642592+Kim-Ziho@users.noreply.github.com>
2024-03-09 16:50:00 +0900

    Title: 행복한 수열의 개수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240309/행복한 수열의 개수/number-of-happy-sequence.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+#define MAX_N 100
+
+using namespace std;
+
+int n, m;
+int arr[MAX_N + 1][MAX_N + 1];
+
+int main() {
+    cin >> n >> m;
+    for (int y = 1; y <= n; y++) {
+        for (int x = 1; x <= n; x++) {
+            cin >> arr[y][x];
+        }
+    }
+    int ans = 0;
+
+    for (int y = 1; y <= n; y++) {
+        int cnt = 0;
+        for (int x = 2; x <= n; x++) {
+            if (arr[y][x] == arr[y][x - 1]) cnt++;
+            else cnt = 0;
+            if (cnt >= m - 1) {
+                ans++;
+                break;
+            }
+        }
+    }
+    for (int x = 1; x <= n; x++) {
+        int cnt = 0;
+        for (int y = 2; y <= n; y++) {
+            if (arr[y][x] == arr[y - 1][x]) cnt++;
+            else cnt = 0;
+            if (cnt >= m - 1) {
+                ans++;
+                break;
+            }
+        }
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
