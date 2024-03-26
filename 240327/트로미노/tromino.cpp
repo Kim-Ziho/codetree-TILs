ad91dc993772c5daa10bb20c2c4cee74b14991c1
Kim Jiho <82642592+Kim-Ziho@users.noreply.github.com>
2024-03-27 01:22:31 +0900

    Title: 트로미노, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240327/트로미노/tromino.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int mask[6][3][3] = {
+    {
+        {1, 0, 0},
+        {1, 1, 0},
+        {0, 0, 0}
+    },
+    {
+        {0, 1, 0},
+        {1, 1, 0},
+        {0, 0, 0}
+    },
+    {
+        {1, 1, 0},
+        {0, 1, 0},
+        {0, 0, 0}
+    },
+    {
+        {1, 1, 0},
+        {1, 0, 0},
+        {0, 0, 0}
+    },
+    {
+        {1, 1, 1},
+        {0, 0, 0},
+        {0, 0, 0}
+    },
+    {
+        {1, 0, 0},
+        {1, 0, 0},
+        {1, 0, 0}
+    },
+};
+
+int arr[201][201];
+int N, M;
+
+int main() {
+    cin >> N >> M;
+
+    for (int y = 1; y <= N; y++) {
+        for (int x = 1; x <= M; x++) {
+            cin >> arr[y][x];
+        }
+    }
+    int max_sum = -21e8;
+
+    for (int y = 1; y <= N; y++) {
+        for (int x = 1; x <= M; x++) {
+            bool out_range = false;
+            for (int i = 0; i < 6; i++) {
+                int sum = 0;
+                bool is_out = false;
+                for (int r = 0; r < 3; r++) {
+                    for (int c = 0; c < 3; c++) {
+                        if (mask[i][r][c] == 0) continue;
+                        int py = y + r;
+                        int px = x + c;
+                        if (py > N || px > N) is_out = true;
+                        sum += arr[py][px];
+                    }
+                }
+                if (is_out) continue;
+                max_sum = max(max_sum, sum);
+            }
+        }
+    }
+
+    cout << max_sum << endl;
+
+    return 0;
+}
